8a580e190ee9785f81c8b1a8bbb50f325711d1e9
Davi Perdigão <davivcperdigao@gmail.com>
2022-07-21 08:47:21 -0300

    Debugging OK

---
+++ JAVA/Debugging JAVA/CalculadoraDeMedias.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CalculadoraDeMedias {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] alunos = {"Camila", "Lucas", "Bruna", "Pedro"};
+
+        double media = calculaMediaDaTurma(alunos, scan);
+
+        System.out.printf("Média da turma %.1f", media);
+    }
+
+    public static double calculaMediaDaTurma(String[] alunos, Scanner scanner) {
+
+        double soma = 0;
+        for(String aluno : alunos) {
+            System.out.printf("Nota do aluno %s: ", aluno);
+            double nota = scanner.nextDouble();
+            soma += nota;
+        }
+
+        return soma / alunos.length;
+    }
+
+}
---
+++ JAVA/Debugging JAVA/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Iniciou do programa no método main.");
+        a();
+        System.out.println("Finalizou do programa no método main.");
+    }
+
+    static void a() {
+        System.out.println("Entrou no método a.");
+        b();
+        System.out.println("Finalizou o método a.");
+    }
+
+    static void b() {
+        System.out.println("Entrou no método b.");
+        for(int i = 0; i <= 4; i++) System.out.println(i);
+        c();
+        System.out.println("Finalizou o método b.");
+    }
+
+    static void c(){
+        System.out.println("Entrou no método c.");
+        //Thread.dumpStack();
+        System.out.println("Finalizou o método c.");
+    }
+
+}
